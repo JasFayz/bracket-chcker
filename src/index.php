5af3495cd466174aeadaf8e1249e51a9c3a784d3
jasur <jasfayz@gmail.com>
2023-05-22 11:12:41 +0500

    Add gitignore

--- src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use JAkhmatfaezov\BracketChecker\Bracket;
-
-$bracket = new Bracket('((((())()(()))))(()))))())))');
-
-var_dump($bracket->checker());
